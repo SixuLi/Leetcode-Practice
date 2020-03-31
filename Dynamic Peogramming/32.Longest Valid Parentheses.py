f0d2e322696ff8f51d84a5d69162201787a32534
lsx971106 <940867461@qq.com>
2020-03-31 09:54:59 -0500

    Create 32.Longest Valid Parentheses.py

---
+++ Dynamic Peogramming/32.Longest Valid Parentheses.py
@@ -0,0 +1,26 @@
+# Solution 1: Using Stack
+# For every "(" encounted, we push its index onto stack.
+# For every ")" encounted, we pop the topmost element and subtract the current element's index from the top element of the stack,
+# which gives the length of the currently encounted valid string of parentheses. If while popping the element, the stack becomes
+# empty, we push the current element's index onto the stack. In this way, we keep on calculating the lengths of the valid substrings,
+# and return the length of the longest valid string at the end.
+# Special case to pay attention: we initialized the stack by pushing -1 into it.
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack = [-1]
+        maxlen = 0
+        for i in range(len(s)):
+            if s[i] == "(":
+                stack.append(i)
+            else:
+                if stack:
+                    stack.pop()
+                    if stack:
+                        maxlen = max(maxlen, i-stack[-1])
+                    else:
+                        stack.append(i)
+        return maxlen
