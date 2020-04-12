1d074902c62ad2b8fbb0e10b3be50597b3f7bc4c
lsx971106 <940867461@qq.com>
2020-04-12 10:09:29 -0500

    Create 144.Binary Tree Preorder Traversal.py

---
+++ Tree/144.Binary Tree Preorder Traversal.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+# Solution 1: DFS with Recursion
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        self.dfs(root, res)
+        return res
+
+    def dfs(self, root, res):
+        if not root:
+            return
+        res.append(root.val)
+        self.dfs(root.left, res)
+        self.dfs(root.right, res)
+
+# Solution 2: DFS with Iteration(using Stack)
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        res = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return res
